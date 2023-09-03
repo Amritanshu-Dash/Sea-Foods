568ac1de86fa37ed64b0689c75440bf05d75052d
amritanshu_dash <amritanshudash6677@gmail.com>
2023-09-03 16:02:41 +0530

    Initial Commit

---
+++ Sea Foods/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sea Foods
+//
+//  Created by Amritanshu Dash on 03/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
